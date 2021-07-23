83d82bd0ff6b6e1787ff6eb245969b4361f7b361
Zulemahdz <85319208+Zulemahdz@users.noreply.github.com>
2021-07-22 22:40:40 -0500

    Create HolaMundo.java
    Mi primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,16 @@
+//Este es el Hola Mundo en Java
+
+public class HolaMundo{
+  public static void main(String []args){
+    System.out.println("Hola Mundo");
+  }
+}
+#Hola mundo Python
+print("Hola mundo")
+  
+ //Hola mundo C
+  
+void main(argv[], argc){
+  printf("Hola mundo \n");
+  return;
+}
