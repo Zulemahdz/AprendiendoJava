6db18149126cbd955f28c80262e50c93596ef142
Zulemahdz <85319208+Zulemahdz@users.noreply.github.com>
2021-11-08 21:19:43 -0600

    Bienvenido a la programación en Java

---
+++ Bienvenido1.java
@@ -0,0 +1,13 @@
+//Porgrama para imprimir texto.
+//ZHS 
+
+public class Bienvenido1
+{
+	//empieza en metodo main
+	public static void main(String[] args)
+	{
+		System.out.print( "Bienvenido a la programacion en Java");
+
+	}	// fin del metodo main
+
+} // fin de la clase BIenvenido1
