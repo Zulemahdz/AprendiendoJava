9739a4528a2384af5b4fb1211e2c8992f80d96bb
Zulemahdz <85319208+Zulemahdz@users.noreply.github.com>
2021-11-13 20:19:35 -0600

    Programa2
    Clase de printf

---
+++ Bienvenido2.java
@@ -0,0 +1,20 @@
+//Porgrama para imprimir texto.
+//ZHS 
+
+public class Bienvenido2
+{
+	//empieza en metodo main
+	public static void main(String[] args)
+	{
+		/* \n Nuevas linea
+			\t tabulador
+			\r retorno dearri
+			\\ para imprimir
+			\"
+		*/
+		
+		System.out.printf("%s\t%s\n", "Bienvenido a", "la casa de Miguel");
+
+	}	// fin del metodo main
+
+} // fin de la clase BIenvenido1
